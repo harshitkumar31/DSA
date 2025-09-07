4913fa7b872afdb4c257d57cb72a34d6b254384b
Harshit Kumar <harshit.kumar@walmart.com>
2025-09-07 16:13:15 +0530

    solved KthSmallestElementInMatrix

--- Solutions/src/KthSmallestElementInMatrix.java
+++ Solutions/src/KthSmallestElementInMatrix.java
@@ -50,4 +50,41 @@ public class KthSmallestElementInMatrix {
         return pq.peek();
 
     }
+
+    public static int kthSmallestElement2(int[][] matrix, int k) {
+        // storing the number of rows in the matrix to use it in later
+        int rowCount = matrix.length;
+        // declaring a min-heap to keep track of smallest elements
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (int i = 0; i < Math.min(rowCount, k); i++) {
+            // pushing the first element of each row in the min-heap
+            // The offer() method pushes an element into an existing heap
+            // in such a way that the heap property is maintained.
+            minHeap.offer(new int[]{matrix[i][0], i, 0});
+        }
+
+        int numbersChecked = 0;
+        int smallestElement = 0;
+        // iterating over the elements pushed in our minHeap
+        while (!minHeap.isEmpty()) {
+            // get the smallest number from top of heap and its corresponding row and column
+            int[] curr = minHeap.poll();
+            smallestElement = curr[0];
+            int rowIndex = curr[1];
+            int colIndex = curr[2];
+            numbersChecked++;
+            // when numbersChecked equals k, we'll return smallestElement
+            if (numbersChecked == k) {
+                break;
+            }
+            // if the current element has a next element in its row,
+            // add the next element of that row to the minHeap
+            if (colIndex + 1 < matrix[rowIndex].length) {
+                minHeap.offer(new int[]{matrix[rowIndex][colIndex + 1], rowIndex, colIndex + 1});
+            }
+        }
+        // return the Kth smallest number found in the matrix
+        return smallestElement;
+    }
 }
